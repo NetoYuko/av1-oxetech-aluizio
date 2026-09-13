72709e98b30deb7da0df5668403dba2faf3d2225
Aluizio Antônio <neto.yuko197@gmail.com>
2026-09-13 14:14:37 -0300

    feat: adiciona livro.py

--- relatorio.py
+++ relatorio.py
@@ -6,7 +6,7 @@ class GeradorRelatorio:
     def relatorio_completo(livros, usuarios):
         logging.info("=== RELATORIO DA BIBLIOTECA ===")
         for id_livro, livro in livros.items():
-            logging.info(f"Livro: {livro['titulo']} | Disponivel: {livro['qtd']}/{livro['qtd_total']}")
+            logging.info(f"Livro: {livro.titulo} | Disponivel: {livro.qtd}/{livro.qtd_total}")
         for id_usuario, usuario in usuarios.items():
             cpf_seguro = mascarar_cpf(usuario.cpf)
             logging.info(f"Usuario: {usuario.nome} CPF: {cpf_seguro} | Emprestimos: {usuario.emprestimos_ativos}")
